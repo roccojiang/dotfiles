2f0bf59703b5a3952663f9042dd34fb354dad1ed
Rocco Jiang <10642716+roccojiang@users.noreply.github.com>
2026-01-13 22:13:35 +0000

    add delta and difftastic for git

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -17,12 +17,19 @@ abbr vi nvim
 # git
 abbr g git
 
-# dynamically generate abbreviations for all git aliases defined in ~/.config/git/config
+# dynamically generate abbreviations for all git aliases defined in ~/.config/git/config,
+# except for those defined in the below git_alias_exclusions list 
 # inspired by https://github.com/timomeh/dotfiles/blob/1500c4bbfc4d524a71fc7242b2a4acf6fdedf16c/.config/fish/config.fish#L30-L33
+set git_alias_exclusions dlog dshow ddiff
+
 for line in (git config -l | rg alias | cut -c 7-)
     set parts (string split -m 1 '=' $line)
     set alias $parts[1]
     set cmd $parts[2]
+
+    if contains $alias $git_alias_exclusions
+        continue
+    end
 
     abbr "g$alias" "git $cmd"
     abbr --command={git,dotfiles} $alias $cmd
