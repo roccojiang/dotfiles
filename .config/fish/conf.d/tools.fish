271b6ead2e9a3d4e9af4dea83c153174a46969de
Rocco Jiang <10642716+roccojiang@users.noreply.github.com>
2026-02-02 23:32:08 +0000

    chore(fish): split up environment and tools configs

---
+++ .config/fish/conf.d/tools.fish
@@ -0,0 +1,19 @@
+# coursier
+fish_add_path "$HOME/Library/Application Support/Coursier/bin"
+
+# ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+fish_add_path "$HOME/.cabal/bin"
+fish_add_path --append --path "$HOME/.ghcup/bin"
+
+# rustup
+source "$HOME/.cargo/env.fish"
+
+# uv
+source "$HOME/.local/bin/env.fish"
+
+# jetbrains
+fish_add_path "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
+
+# orbstack
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
