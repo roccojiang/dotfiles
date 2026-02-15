c6309f445c5e81f319f70857e6bc2182f24e0895
Rocco Jiang <10642716+roccojiang@users.noreply.github.com>
2026-02-15 23:33:47 +0000

    tools(fish): add dedicated tokyonight theme file

--- .config/fish/conf.d/theme.fish
+++ .config/fish/conf.d/theme.fish
@@ -1,16 +1,4 @@
 status is-interactive; or exit
 
-# Tokyo Night theme, adapted from https://github.com/folke/dot/blob/master/config/fish/conf.d/tokyonight.fish
-
-set style moon
-
-set theme tokyonight_{$style}
-
-set src ~/.local/share/nvim/lazy/tokyonight.nvim/extras/fish_themes/{$theme}.theme
-set dst ~/.config/fish/themes/{$theme}.theme
-
-if not test -L $dst
-    ln -s $src $dst
-end
-
-fish_config theme choose $theme
+# Tokyo Night theme, see ../themes/tokyonight.theme
+fish_config theme choose tokyonight
