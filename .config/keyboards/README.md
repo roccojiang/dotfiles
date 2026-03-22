87a9a065ee916634ad2efa80e467d21c65a14186
Rocco Jiang <10642716+roccojiang@users.noreply.github.com>
2026-03-22 22:19:50 +0000

    tools(keyboards): update and move karabiner configs, add qk75 layout file
    - Add a fn keys layer to QK75, so that it can send both media controls
      (by default) and fn keys (using a remapped fn20 key)
    - Add corresponding QK75 layout file for QK Config to preserve this
      layout for use with Karabiner

---
+++ .config/keyboards/README.md
@@ -0,0 +1,19 @@
+# Keyboard config files
+
+## [karabiner-ts](karabiner-ts/)
+
+[Karabiner-Elements](https://karabiner-elements.pqrs.org/) configuration, using [karabiner.ts](https://github.com/evan-liu/karabiner.ts).
+
+### Quick start
+
+```sh
+npm install       # install dependencies
+npm run build     # build & apply to Karabiner profile
+```
+
+## [qk75](qk75/)
+
+Laytout exports from [QK Config for Mac](https://www.qwertykeys.com/pages/fw) for the [QK75 Wireless PCB](https://qwertykeys.notion.site/QK75-Wireless-PCB-Guide-bfb296f823c3438fb07e2c2f6adf7e52).
+Last updated to [QK75 Mac V1.09](https://www.qwertykeys.com/pages/fw) firmware.
+
+To restore: import [`layout.xml`](qk75/layout.xml) into QK Config and flash to the keyboard.
---
+++ .config/keyboards/karabiner-ts/src/index.ts
@@ -0,0 +1,49 @@
+import {
+  ifApp,
+  ifDevice,
+  layer,
+  map,
+  rule,
+  writeToProfile,
+} from 'karabiner.ts'
+
+const HHKB_DEVICE = { vendor_id: 1278, product_id: 33 }
+const QK75_DEVICE = [
+  { vendor_id: 1452, product_id: 591 },   // BLE connection
+  { vendor_id: 3141, product_id: 32791 }, // USB/native connection
+]
+
+writeToProfile('Default', [
+
+  rule('Caps Lock → Control/Escape').manipulators([
+    map('caps_lock', { optional: 'any' }).to('left_control', undefined, { lazy: true }).toIfAlone('escape'),
+  ]),
+
+  rule('Left Control → Hyper').manipulators([
+    map('left_control').toHyper()
+      .condition(ifApp('Terraria').unless()), // ctrl-click in Terraria
+  ]),
+
+  rule('HHKB Control -> Control/Escape', ifDevice(HHKB_DEVICE, 'HHKB-Hybrid_1')).manipulators([
+    map('left_control', { optional: 'any' }).to('left_control', undefined, { lazy: true }).toIfAlone('escape'),
+  ]),
+
+  // See: https://karabiner-elements.pqrs.org/docs/help/how-to/function-keys/
+  layer('f20', 'QK75 Fn Keys') // right split-backspace key mapped to F20 in QK Config
+    .condition(ifDevice(QK75_DEVICE, 'QK75 KB (BLE/USB)'))
+    .configKey((key) => key.toIfAlone('vk_none'), true)
+    .manipulators([
+      map('f1').to('f1', 'fn'),
+      map('f2').to('f2', 'fn'),
+      map('f3').to('f3', 'fn'),
+      map('f4').to('f4', 'fn'),
+      map('f5').to('f5', 'fn'),
+      map('f6').to('f6', 'fn'),
+      map('f7').to('f7', 'fn'),
+      map('f8').to('f8', 'fn'),
+      map('f9').to('f9', 'fn'),
+      map('f10').to('f10', 'fn'),
+      map('f11').to('f11', 'fn'),
+      map('f12').to('f12', 'fn'),
+    ]),
+])
